adccd9b3c84460e14ff9c0adbfdd9e485d76c5bb
Gopi9398 <vallabhanenigopi4@gmail.com>
2024-07-29 18:01:58 +0530

    fivth push

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ resource "spotify_playlist" "kalki-playlist" {
   description = "My playlist is so awesome"
   tracks = ["3z8DnKyVl4TVZHT2KOHHpw"]
 }
+
+data "spotify_search_track" "ariana_grande" {
+  artist = "Thaman"
+}
+
+resource "spotify_playlist" "demo" {
+    name = "test"
+    tracks = [data.spotify_search_track.Thaman.tracks[0].id,
+    data.spotify_search_track.Thaman.tracks[1].id,
+    data.spotify_search_track.Thaman.tracks[4].id]
+   
+}
