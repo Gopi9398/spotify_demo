53a28d690448bfb07bd1f030b2cd5850521784f1
Gopi9398 <vallabhanenigopi4@gmail.com>
2024-07-29 17:35:52 +0530

    second push

--- provider.tf
+++ provider.tf
@@ -8,11 +8,5 @@ terraform {
 }
 
 provider "spotify" {
-  api_key = var.spotify_api_key
-}
-
-# See https://github.com/conradludgate/terraform-provider-spotify#how-to-use
-# for how to get an api key
-variable "spotify_api_key" {
-  type = string
+  api_key = var.api_key
 }
---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+api_key = "S1dWS-kctpsG4ZVj4MrEejiRdPGwpDBwXyz3rNHrOf9WEpIyTF4XbMRJOrznkF-B"
