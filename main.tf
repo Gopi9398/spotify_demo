409269c2722c750c723b398fa87e76394d4d3603
Gopi9398 <vallabhanenigopi4@gmail.com>
2024-07-29 17:47:35 +0530

    fourth push

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "spotify_playlist" "gym-playlist" {
-  name        = "Gym-playlist"
+resource "spotify_playlist" "kalki-playlist" {
+  name        = "kalki-playlist"
   description = "My playlist is so awesome"
   tracks = ["3z8DnKyVl4TVZHT2KOHHpw"]
 }
